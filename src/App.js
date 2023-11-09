4546b917cec01fbb6674535b40e71959b5fabc88
Roshan Yadav <crazyopuser@outlook.com>
2023-11-09 14:46:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import axios from 'axios';
 import { useRef, useState } from 'react';
 import './App.css';
-import { youtube_parser } from './utils';
 
 function App() {
   const inputUrlRef=useRef();
   const [data, setData] = useState(null);
 
-  const handleSubmit = (e)=> {
+  const handleSubmit = async (e)=> {
     e.preventDefault();
     console.log(inputUrlRef.current.value);
-    const ytID = youtube_parser(inputUrlRef.current.value);
-    console.log(ytID);
+    const igID = inputUrlRef.current.value;
 
     const options = {
       method: 'GET',
-      url: 'https://youtube-mp36.p.rapidapi.com/dl',
-      params: {id: ytID},
+      url: 'https://instagram-downloader-download-instagram-videos-stories.p.rapidapi.com/index',
+      params: {
+        url: igID,
+      },
       headers: {
         'X-RapidAPI-Key': '461c9e292emsh2968541a1ad61c0p14062cjsnc34da2e8fad4',
-        'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+        'X-RapidAPI-Host': 'instagram-downloader-download-instagram-videos-stories.p.rapidapi.com'
       }
     };
-    axios(options)
-    .then(res=>setData(res.data.link))
-    .catch(err=>console.log(err))
+
+    try {
+      const response = await axios.request(options);
+      if(igID.includes('stories')){
+        setData(response.data.story_by_id.media)
+      } else {
+        setData(response.data.media)
+      }
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
 
     inputUrlRef.current.value='';
   }
@@ -32,15 +41,15 @@ function App() {
   return (
     <div className="app">
       <section className='content'>
-        <h1 className='content-title'>YouTube to MP3 Converter</h1>
+        <h1 className='content-title'>Instagram Media Downloader</h1>
         <p className='content-desc'>
-          Transform YouTube videos into MP3s in just a click!
+          Download Reels & Stories in just a click!
         </p>
         <form className='form' onSubmit={handleSubmit}>
-          <input ref={inputUrlRef} type="text" placeholder='Paste a Youtube video URL link...' className='form-input'/>
+          <input ref={inputUrlRef} type="text" placeholder='Paste a URL link...' className='form-input'/>
           <button type='submit' className='form-btn'>Search</button>
         </form>
-        {data ? <a target='_blank' rel='noreferrer' href={data} className='download-btn'>Download MP3</a> : ''}
+        {data ? <a target='_blank' rel='noreferrer' href={data} className='download-btn'>Download</a> : ''}
       </section>
     </div>
   );
